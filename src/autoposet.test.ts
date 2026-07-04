70036e0c739e2db3a6173334ad25be27cba9fada
eguneys <eguneys@gmail.com>
2026-07-04 08:17:18 +0300

    160 ok

--- src/autoposet.test.ts
+++ src/autoposet.test.ts
@@ -14,10 +14,10 @@ import { Debug, Fen, Files, Position, Ranks } from './types.js'
 
 let puzzles = read_csv(fs.readFileSync('data/athousand_sorted.csv').toString())
 let puzzles100 = puzzles.slice(0, 100).reverse()
-let puzzles200 = puzzles.slice(0, 150).reverse()
+let puzzles200 = puzzles.slice(150, 160).reverse()
 
-let fundamentals = read_oof(fs.readFileSync('data/more100.oof').toString())
-let more200 = read_oof(fs.readFileSync('data/more200.oof').toString())
+let fundamentals = read_oof(fs.readFileSync('data/more100.oof').toString().trim())
+let more200 = read_oof(fs.readFileSync('data/more200.oof').toString().trim())
 
 function read_oof(oof: string): [string, string][] {
     let result: [string, string][] = []
