5b26435840d23dd1faa4940a56f94a9330505b6a
eguneys <eguneys@gmail.com>
2026-07-05 20:54:27 +0300

    240

--- src/autoposet.test.ts
+++ src/autoposet.test.ts
@@ -15,7 +15,7 @@ import { ScriptFilter, ScriptRunner } from './runner.js'
 import { Debug, Fen, Files, Position, Ranks } from './types.js'
 
 let puzzles = read_csv(fs.readFileSync('data/athousand_sorted.csv').toString())
-let puzzles200 = puzzles.slice(200, 230).reverse()
+let puzzles200 = puzzles.slice(200, 240).reverse()
 
 let fundamentals = read_oof(fs.readFileSync('data/more100.oof').toString().trim())
 let more200 = read_oof(fs.readFileSync('data/more200.oof').toString().trim())
